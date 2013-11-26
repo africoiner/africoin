5b7262fc67424fea5a04209ea34792a206d007d2
africoiner <africoiner@kr105.com>
2013-11-26 04:18:20 -0300

    x

--- src/main.cpp
+++ src/main.cpp
@@ -4519,7 +4519,7 @@ void static BitcoinMiner(CWallet *pwallet)
     CReserveKey reservekey(pwallet);
     unsigned int nExtraNonce = 0;
 
-    try { loop {
+    try { while(true) {
         while (vNodes.empty())
             MilliSleep(1000);
 
@@ -4559,7 +4559,7 @@ void static BitcoinMiner(CWallet *pwallet)
         uint256 hashTarget = CBigNum().SetCompact(pblock->nBits).getuint256();
         uint256 hashbuf[2];
         uint256& hash = *alignup<16>(hashbuf);
-        loop
+        while(true)
         {
             unsigned int nHashesDone = 0;
             unsigned int nNonceFound;
